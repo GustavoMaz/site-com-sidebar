ad4e8e4573c591314aa920786bca3bb85d65a06d
Gustavo Mazurkiewitz <gmazurkiewitz@gmail.com>
2024-02-27 21:28:12 -0300

    Verificação no campo de nome

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-document.querySelector("input[type='submit']").addEventListener('click', validateInput());
+document.getElementById('submit').addEventListener('click', validateInput);
 function validateInput() {
     var name = document.getElementById('name');
+    var email = document.getElementById('email');
+    var password = document.getElementById('password');
+    var passwordConfirm = document.getElementById('password-confirm');
+
+    if (name.value.length == 0) {
+        window.alert('Por favor, preencha todos os campos');
+    }
 }

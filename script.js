a6520922794a6b9f7856073fb1168880a4798cfc
Gustavo Mazurkiewitz <gmazurkiewitz@gmail.com>
2024-02-28 17:39:19 -0300

    Validação do formulário e estilização das mensagens de erro

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
-document.getElementById('submit').addEventListener('click', validateInput);
+document.getElementById('btnsubmit').addEventListener('click', validateInput);
+
 function validateInput() {
     var name = document.getElementById('name');
     var email = document.getElementById('email');
     var password = document.getElementById('password');
     var passwordConfirm = document.getElementById('password-confirm');
 
-    if (name.value.length == 0) {
+    if (isEmpty(name) || isEmpty(email) || isEmpty(password) || isEmpty(passwordConfirm)) {
         window.alert('Por favor, preencha todos os campos');
+    }
+
+    if (name.value.length < 3) {
+        name.className = 'invalid'
+        document.querySelector('#name + div').className = 'invalid msg'; //Seleciona a div imediatamente após o elemento
+    }
+
+    if (email.value.includes('@') == false ) {
+        email.className = 'invalid'
+        document.querySelector('#email + div').className = 'invalid msg';
+    }
+
+    if (password.value.length < 8) {
+        password.className = 'invalid'
+        document.querySelector('#password + div').className = 'invalid msg';
+    }
+
+    if (passwordConfirm.value != password.value) {
+        passwordConfirm.className = 'invalid'
+        document.querySelector('#password-confirm + div').className = 'invalid msg';
+    }
+}
+
+function isEmpty(elem) {
+    if (elem.value.length == 0) {
+        return true;
+    } else {
+        return false;
     }
 }

7cfd89475dad643fa0fbfe6f4b8aac92330112fe
Gustavo Mazurkiewitz <gmazurkiewitz@gmail.com>
2024-02-28 19:58:31 -0300

    Sidebar funcional e layout responsivo

--- toggle-sidebar.js
+++ toggle-sidebar.js
@@ -1,5 +1,18 @@
 document.getElementById('btnsidebar').addEventListener("click", toggleSidebar);
+var sidebar = 0;
 
 function toggleSidebar() {
-    document.querySelector('nav').style.left = 0;
+    var nav = document.querySelector('nav');
+    var container = document.getElementById('form-container');
+    if (sidebar == 0 ) {
+        nav.style.left = 0;
+        container.style.filter = 'brightness(60%)';
+        container.style.backdropFilter = 'brightness(60%)';
+        sidebar = 1;
+    } else {
+        nav.style.left = '-30vw';
+        container.style.filter = 'brightness(100%)';
+        container.style.backdropFilter = 'brightness(100%)';
+        sidebar = 0;
+    }
 }

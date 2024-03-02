f2e1829f01010bd8df2d40814acd221fb0114a44
Gustavo Mazurkiewitz <gmazurkiewitz@gmail.com>
2024-03-02 09:58:03 -0300

    Ajustes no layout mobile

--- toggle-sidebar.js
+++ toggle-sidebar.js
@@ -10,7 +10,7 @@ function toggleSidebar() {
         container.style.backdropFilter = 'brightness(60%)';
         sidebar = 1;
     } else {
-        nav.style.left = '-30vw';
+        nav.style.left = '-100vw';
         container.style.filter = 'brightness(100%)';
         container.style.backdropFilter = 'brightness(100%)';
         sidebar = 0;

abbbbfd418a52c919e006adc90d84c2e47a99f07
Gustavo Mazurkiewitz <gmazurkiewitz@gmail.com>
2024-03-03 10:05:09 -0300

    Adicionando o atributo readonly aos inputs quando o menu sidebar estiver aberto

--- toggle-sidebar.js
+++ toggle-sidebar.js
@@ -3,23 +3,21 @@ var sidebar = 0;
 var inputs = document.querySelectorAll('form .typing-input, input[type="radio"]');
 
 function toggleSidebar() {
-    var nav = document.querySelector('nav');
-    var container = document.getElementById('form-container');
+    let nav = document.querySelector('nav');
+    let container = document.getElementById('form-container');
     if (sidebar == 0 ) {
         nav.style.left = 0;
         container.style.filter = 'brightness(60%)';
         container.style.backdropFilter = 'brightness(60%)';
-        for (i in inputs) {
-            inputs[i].setAttribute(readonly, 'readonly');
-        }
         sidebar = 1;
     } else {
         nav.style.left = '-100vw';
         container.style.filter = 'brightness(100%)';
         container.style.backdropFilter = 'brightness(100%)';
-        for (i in inputs) {
-            inputs[i].setAttribute(readonly, 'false');
-        }
         sidebar = 0;
+    }
+
+    for (i in inputs) {
+        inputs[i].toggleAttribute('readonly');
     }
 }

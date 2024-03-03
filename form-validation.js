0f0eeb06218ffa5a4cc93144cbc762037c2be315
Gustavo Mazurkiewitz <gmazurkiewitz@gmail.com>
2024-03-03 15:17:30 -0300

    Fazendo com que as mensagens de erro sumam quando o input for válido

--- form-validation.js
+++ form-validation.js
@@ -5,29 +5,45 @@ function validateInput() {
     var email = document.getElementById('email');
     var password = document.getElementById('password');
     var passwordConfirm = document.getElementById('password-confirm');
+    var nameMsg = document.querySelector('#name + div.msg'); //Seleciona a div imediatamente após o elemento
+    var emailMsg = document.querySelector('#email + div.msg');
+    var passwordMsg = document.querySelector('#password + div.msg');
+    var passwordConfirmMsg = document.querySelector('#password-confirm + div.msg');
 
     if (isEmpty(name) || isEmpty(email) || isEmpty(password) || isEmpty(passwordConfirm)) {
         window.alert('Por favor, preencha todos os campos');
     }
 
     if (name.value.length < 3) {
-        name.className = 'invalid'
-        document.querySelector('#name + div').className = 'invalid msg'; //Seleciona a div imediatamente após o elemento
+        name.classList.add('invalid');
+        nameMsg.classList.add('invalid');
+    } else { 
+        name.classList.remove('invalid');
+        nameMsg.classList.remove('invalid');
     }
 
     if (email.value.includes('@') == false ) {
-        email.className = 'invalid'
-        document.querySelector('#email + div').className = 'invalid msg';
+        email.classList.add('invalid');
+        emailMsg.classList.add('invalid');
+    } else { 
+        email.classList.remove('invalid');
+        emailMsg.classList.remove('invalid');
     }
 
     if (password.value.length < 8) {
-        password.className = 'invalid'
-        document.querySelector('#password + div').className = 'invalid msg';
+        password.classList.add('invalid');
+        passwordMsg.classList.add('invalid');
+    } else { 
+        password.classList.remove('invalid');
+        passwordMsg.classList.remove('invalid');
     }
 
     if (passwordConfirm.value != password.value) {
-        passwordConfirm.className = 'invalid'
-        document.querySelector('#password-confirm + div').className = 'invalid msg';
+        passwordConfirm.classList.add('invalid');
+        passwordConfirmMsg.classList.add('invalid');
+    } else {
+        passwordConfirm.classList.remove('invalid');
+        passwordConfirmMsg.classList.remove('invalid');
     }
 }
 

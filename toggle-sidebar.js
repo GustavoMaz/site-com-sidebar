dce4d9958d50f6d7ca8bfb7c9af9b006be8fe261
Gustavo Mazurkiewitz <gmazurkiewitz@gmail.com>
2024-03-04 19:51:47 -0300

    Tocar fora do menu sidebar fecha ele

--- toggle-sidebar.js
+++ toggle-sidebar.js
@@ -1,19 +1,21 @@
-document.getElementById('btnsidebar').addEventListener("click", toggleSidebar);
+document.getElementById('btnsidebar').addEventListener('click', toggleSidebar);
 var sidebar = 0;
-var inputs = document.querySelectorAll('form .typing-input, input[type="radio"]');
+var inputs = document.querySelectorAll('form .typing-input');
 
 function toggleSidebar() {
     let nav = document.querySelector('nav');
-    let container = document.getElementById('form-container');
+    let formContainer = document.getElementById('form-container');
     if (sidebar == 0 ) {
         nav.style.left = 0;
-        container.style.filter = 'brightness(60%)';
-        container.style.backdropFilter = 'brightness(60%)';
+        formContainer.style.filter = 'brightness(60%)';
+        formContainer.style.backdropFilter = 'brightness(60%)';
+        formContainer.addEventListener('click', toggleSidebar);
         sidebar = 1;
     } else {
         nav.style.left = '-100vw';
-        container.style.filter = 'brightness(100%)';
-        container.style.backdropFilter = 'brightness(100%)';
+        formContainer.style.filter = 'brightness(100%)';
+        formContainer.style.backdropFilter = 'brightness(100%)';
+        formContainer.removeEventListener('click', toggleSidebar);
         sidebar = 0;
     }
 
